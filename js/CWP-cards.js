6dc8236c3935e92bfdd06289ce40d53a928ae8db
Chris Myers <chrismyers@Chriss-MacBook-Pro.local>
2020-11-21 23:15:29 -0500

    commit

--- js/CWP-cards.js
+++ js/CWP-cards.js
@@ -1,13 +1,14 @@
 
 
-function getPoems(){
+function writeCards(){
     //for live
     //let poemjson = $.getJSON("../poems/CWPP.json");
 
     //for local
-    let rawjson = $.getJSON("https://raw.githubusercontent.com/ChristopherJonMyers/christopherjonmyers.github.io/master/poetry/poems/CWPP.json");
-    let jsonobj = JSON.parse(rawjson);
-    window.alert(jsonobj);
+    let poemsjson = $.getJSON("https://raw.githubusercontent.com/ChristopherJonMyers/christopherjonmyers.github.io/master/poetry/poems/CWPP.json");
+    for( i in poemsjson){
+        
+    }
 }
 
-getPoems();
+writeCards();

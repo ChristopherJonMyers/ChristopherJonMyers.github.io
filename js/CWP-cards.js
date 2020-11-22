402f965f6bc9b9eadfeb5a7e1bad28cb524192a9
Chris Myers <chrismyers@Chriss-MacBook-Pro.local>
2020-11-22 00:32:44 -0500

    why

--- js/CWP-cards.js
+++ js/CWP-cards.js
@@ -4,12 +4,24 @@ function writeCards(){
     
     let url = 'https://raw.githubusercontent.com/ChristopherJonMyers/christopherjonmyers.github.io/master/poetry/poems/CWPP.json';
 
-    fetch(url)
-    .then(res => res.json())
-    .then((out) => {
-            console.log('Checkout this JSON! ', out);
-    })
-    .catch(err => { throw err });
+    var poemsTitles = [];
+    var poemsText = [];
+    $.getJSON(url, function(data) {
+        $.each(data, function(index){
+            poemsTitles.push(data[index].Title);
+            poemsText.push(data[index].Text);
+
+            document.getElementById('poems-cards').innerHTML += '<div class="card mx-auto shadow" style="width: 18rem;">'
+                                                                    +'<div class="col-xs-12 col-sm-6 col-lg-4">'
+                                                                    +'<div class="card-body">'
+                                                                        +'<h5 class="card-title">' + poemsTitles[index] + '</h5>' 
+                                                                        +'<a href="../poetry/poems/poem.html?name="' + poemsTitles[index] + '" class="btn btn-primary">Read</a>'
+                                                                        +'<div/><div/><div/><div/>';
+
+        });
+    });
+
+   
 }
 
 writeCards();

a52343997bc0d8c4532c913f83bc37f2c2af1705
Chris Myers <funbuckethd@gmail.com>
2020-12-11 16:21:07 -0500

    Addded Related poems nav

--- js/poem-data.js
+++ js/poem-data.js
@@ -3,28 +3,29 @@ const params = new URLSearchParams(queryString);
 const poemName = params.get('name');
 const project = params.get('page');
 
+var url;
+var backurl;
+
+switch(project){
+    case 'CWPP':
+        url = 'https://raw.githubusercontent.com/ChristopherJonMyers/christopherjonmyers.github.io/master/poetry/poems/CWPP.json';
+        backurl = "../projects/Creative-Writing-Portfolio.html#Poems";
+        break;
+    case 'Week1':
+        url = 'https://raw.githubusercontent.com/ChristopherJonMyers/christopherjonmyers.github.io/master/poetry/poems/Week1.json';
+        backurl = "../projects/HTWP/Week-1.html#Poems";
+        break;
+}
+
 function writeTitle(){
     document.getElementById('Title').innerHTML = poemName;
 }
 
-function setBackURL(backURL){
+function setBackURL(backurl){
     document.getElementById('Back-Button').href = backurl;
 }
 
-function writePoem(page){
-    var url;
-
-    switch(page){
-        case 'CWPP':
-            url = 'https://raw.githubusercontent.com/ChristopherJonMyers/christopherjonmyers.github.io/master/poetry/poems/CWPP.json';
-            backurl = "../projects/Creative-Writing-Portfolio.html#Poems";
-            break;
-        case 'Week1':
-            url = 'https://raw.githubusercontent.com/ChristopherJonMyers/christopherjonmyers.github.io/master/poetry/poems/Week1.json';
-            backurl = "../projects/HTWP/Week-1.html#Poems";
-            break;
-    }
-
+function writePoem(){
     $.getJSON(url, function(data) {
         $.each(data, function(index){
             if(data[index].Title == poemName){
@@ -33,7 +34,17 @@ function writePoem(page){
         });
     });  
     writeTitle();
-    setBackURL(backurl);
 }
 
-writePoem(project);
+function writeOther(){
+    $.getJSON(url, function(data) {
+        $.each(data, function(index){
+            if(data[index].Title != poemName){
+                document.getElementById('otherPoems').innerHTML +=  '<a href="poems.html?page=' + project + '&name='+ data[index].Title + '"' + 'class="list-group-item list-group-item-action">' + data[index].Title + '<a/>';
+            }
+        });
+    });  
+}
+
+writePoem();
+writeOther();
